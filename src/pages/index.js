4358bf839f6ff41aa217ee95a1e8b8ead6b9ca30
Andrei Sosnin <andrei.sosnin@gmail.com>
2020-06-09 22:04:07 +0300

    Cleanup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,11 @@
 import React from 'react'
 
 import { graphql, useStaticQuery } from 'gatsby'
-//import { makeStyles } from '@material-ui/core/styles'
-import Grid from '@material-ui/core/Grid'
-import Divider from '@material-ui/core/Divider'
 
 import SlotMachine from '../components/slot'
 import Layout from '../components/layout'
-import Image from '../components/image'
 import SEO from '../components/seo'
 
-//import { Typography } from '@material-ui/core'
-
-/*
-const useStyles = makeStyles(theme => ({
-  root: {
-    width: '100%',
-    backgroundColor: theme.palette.background.paper,
-  },
-  nested: {
-    paddingLeft: theme.spacing(4),
-  },
-}))
-*/
 
 const IndexPage = () => {
   const data = useStaticQuery(graphql`
@@ -38,6 +21,7 @@ const IndexPage = () => {
     bar3: file(relativePath: { eq: "3xBAR.png" }) {
       ...reelImage
     }
+    
     bar2: file(relativePath: { eq: "2xBAR.png" }) {
       ...reelImage
     }
@@ -49,24 +33,10 @@ const IndexPage = () => {
   }
 `)
 
-  //const classes = useStyles()
-
   return (
     <Layout>
       <SEO title="Home" />
-      <Grid container spacing={3} justify="center">
-        <Grid item xs={2}>
-          <div style={{ maxWidth: `100px`, marginBottom: `1.45rem` }}>
-            <Image />
-          </div>
-        </Grid>
-        <Grid item xs={8}>
-          <h1>Material Slot Machine</h1>
-        </Grid>
-      </Grid>
-      <Divider />
-      <SlotMachine stroke={null} resources={data} /> 
-      
+      <SlotMachine resources={data} />
     </Layout>
   )
 }
